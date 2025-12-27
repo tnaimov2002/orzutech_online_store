2bd005b2d8f45dc1e65fc098bee7fb535725baa7
To'lqinjon <tnaimov2002@gmail.com>
2025-12-27 16:36:16 +0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Geologica', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
